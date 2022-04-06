d0fce70e2e6e57df81a69afdd9ee67103478e400
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2022-04-06 13:54:15 -0500

    [+] Ejercicio 1 - Tema 4: Control de Flujo - Mostrar si es mayor o menor de edad

---
+++ e1-t4.py
@@ -0,0 +1,8 @@
+# Escribe un programa que pregunte al usuario su edad y muestre por pantalla si es mayor de edad o no.
+age = int( input( 'Digite su edad: ' ) )
+
+# En Colombia se es mayor de edad con 18 años
+if age > 19 :
+    print( 'Eres mayor de edad' )
+else :
+    print( 'Eres menor de edad' )
